d9b9e1af90bd8ab2f15e928c03770c445fb77b5a
Oliver Klukas <oliverklukas@t-online.de>
2021-12-19 11:12:17 +0100

    update README.md

---
+++ src/screens/FinalScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class FinalScreen extends React.Component {
+    render(){
+        return(
+            <div>
+                Final Screen.
+            </div>
+        );
+    }
+}
