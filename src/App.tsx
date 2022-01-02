098696c909c000ddad15cf0f75b1a660b26fd006
Oliver Klukas <oliverklukas@t-online.de>
2022-01-02 11:45:17 +0100

    successfully transitioned to tailwind, typscript & react

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import React from 'react';
 import './App.css';
-import {LandingScreen} from "./screens/LandingScreen";
 
 function App() {
-    return (
-        <div>
-            <LandingScreen />
-        </div>
-    );
+  return (
+    <div className="App">
+        <h1 className="text-4xl font-bold">
+            Work. In. Progress.
+        </h1>
+        <h2 className="text-xl font-bold">
+            Please revisit later, cheers!
+        </h2>
+    </div>
+  );
 }
 
 export default App;
