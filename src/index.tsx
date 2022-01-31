2c112568563d6037aa7560fc509494a086da507a
Oliver Klukas <oliverklukas@t-online.de>
2022-01-31 22:11:36 +0100

    bugfix: font on mobile browsers

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova&display=swap');
+        </style>
         <App/>
     </React.StrictMode>,
     document.getElementById('root')
