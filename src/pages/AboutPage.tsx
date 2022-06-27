fd613dc4e979ef4fb51302fd824142ac36060f35
Oliver Klukas <oliverklukas@t-online.de>
2022-06-27 16:11:35 +0200

    cleaned develop

--- src/pages/AboutPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function AboutPage() {
-    return(
-        <div className="">
-            <p className="text-heading">About.</p>
-        </div>
-    )
-}
