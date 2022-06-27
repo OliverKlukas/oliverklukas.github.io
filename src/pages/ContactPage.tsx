061316fec007c21f205ddcb15f17c1a6ca52cad2
Oliver Klukas <oliverklukas@t-online.de>
2022-06-28 00:21:25 +0200

    Bugfix: links on contact page now new tab

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -11,10 +11,10 @@ export default function ContactPage() {
                 <p className="text-mobile-standard lg:text-standard mt-4 ml-4 mr-4 lg:max-w-4xl">That's it, you've reached the end... at least for now! Do you want to collaborate or talk about an idea? Want to grab a coffee and discuss art, music or code? Then drop me a message on LinkedIn or check out my other projects on GitHub. See ya!</p>
             </div>
             <div className="flex space-x-9 lg:space-x-20">
-                <a href="https://github.com/OliverKlukas">
+                <a href="https://github.com/OliverKlukas" target={"_blank"}>
                     <img className="h-24 lg:h-32 hover:opacity-80" src={github}/>
                 </a>
-                <a href="https://www.linkedin.com/in/oliver-klukas/">
+                <a href="https://www.linkedin.com/in/oliver-klukas/" target={"_blank"}>
                     <div className="h-24 lg:h-32 hover:opacity-80">
                         <LinkedInLogo/>
                     </div>
