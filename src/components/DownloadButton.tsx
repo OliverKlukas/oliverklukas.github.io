1e1d65e8c7ff8cbc4aba46700da16b4a03fde9e3
Oliver Klukas <oliverklukas@t-online.de>
2022-07-11 22:27:13 +0200

    Added drop shadow to buttons

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -8,7 +8,7 @@ import React from 'react';
  */
 export function DownloadButton(props: {title: string, link: string}) {
     return (
-        <a className="bg-white hover:bg-green-4 hover:text-white text-mobile-standard lg:text-standard text-green-5 py-2 lg:py-4 px-4 lg:px-8 border border-green-5 hover:border-green-4 rounded shadow max-w-fit" href={props.link} download>
+        <a className="bg-white hover:bg-green-4 hover:text-white text-mobile-standard drop-shadow-md lg:text-standard text-green-5 py-2 lg:py-4 px-4 lg:px-8 border border-green-5 hover:border-green-4 rounded shadow max-w-fit" href={props.link} download>
             {"Download " + props.title}
         </a>
     )
--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -8,6 +8,6 @@ import React from 'react';
  */
 export function LinkButton(props: { title: string, link: string }) {
     return (
-        <a className="bg-white hover:bg-green-4 hover:text-white text-mobile-standard lg:text-standard text-green-5 py-2 lg:py-4 px-4 lg:px-8 border border-green-5 hover:border-green-4 rounded shadow max-w-fit" target="_blank" rel="noreferrer" href={props.link}>{props.title}</a>
+        <a className="bg-white drop-shadow-md hover:bg-green-4 hover:text-white text-mobile-standard lg:text-standard text-green-5 py-2 lg:py-4 px-4 lg:px-8 border border-green-5 hover:border-green-4 rounded shadow max-w-fit" target="_blank" rel="noreferrer" href={props.link}>{props.title}</a>
     )
 }
