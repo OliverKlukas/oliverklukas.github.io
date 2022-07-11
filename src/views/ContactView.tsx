a1806c1f91beeac1acea37f8dd68ae63c4c8ed2e
Oliver Klukas <oliverklukas@t-online.de>
2022-07-11 21:29:09 +0200

    fixed apos in contact page

--- src/views/ContactView.tsx
+++ src/views/ContactView.tsx
@@ -12,9 +12,8 @@ export default function ContactView() {
         <div className="flex flex-col min-h-screen justify-center space-y-14 lg:space-y-28 items-center">
             <div className="text-center">
                 <p className="text-mobile-heading lg:text-heading">The end.</p>
-                <p className="text-mobile-standard lg:text-standard mt-4 ml-4 mr-4 lg:max-w-4xl">That`&apos`s it,
-                    you`&apos`ve
-                    reached the end... at least for now! Do you want to collaborate or talk about an idea? Want to grab
+                <p className="text-mobile-standard lg:text-standard mt-4 ml-4 mr-4 lg:max-w-4xl">That&apos;s it,
+                    you&apos;ve reached the end... at least for now! Do you want to collaborate or talk about an idea? Want to grab
                     a coffee and discuss art, music or code? Then drop me a message on LinkedIn or check out my other
                     projects on GitHub. See ya!</p>
             </div>
@@ -25,7 +24,6 @@ export default function ContactView() {
                 <a href="https://www.linkedin.com/in/oliver-klukas/" target={"_blank"} rel="noreferrer">
                     <LinkedInLogo className="h-24 lg:h-32 hover:opacity-80 drop-shadow-xl"/>
                 </a>
-
             </div>
         </div>
     )
