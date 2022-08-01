b0f46f3b1db10644a8186b6c82271b1fbd8eecff
Oliver Klukas <oliverklukas@t-online.de>
2022-08-01 09:46:58 +0200

    Cleaned repository

--- src/utils/random.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Returns a random integer value given a max value.
- *
- * @param {number} max - Maximal value the random integer should be able to take.
- * @return {number} Returns random integer value.
- */
-export function getRandomInt(max : number) {
-    return Math.floor(Math.random() * max);
-}
