25d4d60667637c2718c24b688bbfe04e5d418679
Oliver Klukas <oliverklukas@t-online.de>
2022-08-01 13:27:55 +0200

    bugfix: animation of scroll fixed

--- src/views/LandingView.tsx
+++ src/views/LandingView.tsx
@@ -24,7 +24,7 @@ export default function LandingView() {
         tl.to(".text", {text: {value: "23 years old."}, duration: 1.5, delay: 1, repeat: 1, yoyo: true, ease: "none", repeatDelay: 2});
         tl.to(".text", {text: {value: "Passionate about software, learning & bringing ideas to life."}, duration: 4, repeatDelay: 2, repeat: 1, yoyo: true, delay: 1, ease: "none"});
         tl.to(".text", {text: {value: "M.Sc. Information Systems @ TUM & Sorbonne."}, duration: 3, delay: 1, repeat: 1, repeatDelay: 2, yoyo: true, ease: "none"});
-        tl.to(".text", {text: {value: "Scroll to explore."}, duration: 1.5, ease: "none"});
+        tl.to(".text", {text: {value: "Scroll to explore."}, duration: 1.5, delay: 1, ease: "none"});
     })
 
     return (
