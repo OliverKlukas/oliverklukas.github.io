52322384ef96b2f9ded7ffe08bec63f9f3721551
Oliver Klukas <oliverklukas@t-online.de>
2023-03-01 20:34:56 +0100

    Add bouncing arrow back to landing view

--- src/views/LandingView.tsx
+++ src/views/LandingView.tsx
@@ -23,20 +23,19 @@ export default function LandingView() {
     // Trigger animation for texts writing.
     useLayoutEffect(() => {
         firstAnimationActive && gsap.fromTo(".cursor", {autoAlpha: 0, x: -10}, {autoAlpha: 1, duration: 0.7, repeat: -1, ease: "steps (1)"});
-        // tl.to(".text", {text: {value: "Hi there!"}, duration: 1.5, delay: 1,  repeat: 1, yoyo: true, ease: "none", repeatDelay: 2});
-        // tl.to(".text", {text: {value: "I&apos;m Olli."}, duration: 1.5, delay: 1, repeat: 1, yoyo: true, ease: "none", repeatDelay: 2});
-        // tl.to(".text", {text: {value: "24 years old & based in Munich, Germany."}, duration: 3.1, delay: 1, repeat: 1, yoyo: true, ease: "none", repeatDelay: 2});
-        // tl.to(".text", {text: {value: "Passionate about software, learning & bringing ideas to life."}, duration: 4, repeatDelay: 2, repeat: 1, yoyo: true, delay: 1, ease: "none"});
-        // tl.to(".text", {text: {value: "M.Sc. Information Systems @ TUM & Sorbonne Université."}, duration: 3.8, delay: 1, repeat: 1, repeatDelay: 2, yoyo: true, ease: "none"});
+        tl.to(".text", {text: {value: "Hi there!"}, duration: 1.5, delay: 1,  repeat: 1, yoyo: true, ease: "none", repeatDelay: 2});
+        tl.to(".text", {text: {value: "I&apos;m Olli."}, duration: 1.5, delay: 1, repeat: 1, yoyo: true, ease: "none", repeatDelay: 2});
+        tl.to(".text", {text: {value: "24 years old & based in Munich, Germany."}, duration: 3.1, delay: 1, repeat: 1, yoyo: true, ease: "none", repeatDelay: 2});
+        tl.to(".text", {text: {value: "Passionate about software, learning & bringing ideas to life."}, duration: 4, repeatDelay: 2, repeat: 1, yoyo: true, delay: 1, ease: "none"});
+        tl.to(".text", {text: {value: "M.Sc. Information Systems @ TUM & Sorbonne Université."}, duration: 3.8, delay: 1, repeat: 1, repeatDelay: 2, yoyo: true, ease: "none"});
         tl.to(".text", {text: {value: "Scroll to explore."}, duration: 1.5, delay: 1, ease: "none", onComplete: () => setFirstAnimationActive(false)});
-        tl.to(".bouncer", {opacity: 1})
+        tl.to(".bouncer", {opacity: 1, duration: 5, ease: "power2"});
     }, []);
 
     return (
-        <div>
-            <div className="grid min-h-screen bg-green-4 content-around lg:grid-cols-2 lg:justify-center lg:items-center">
-                <div className="flex justify-center py-16 mx-16 lg:justify-end lg:mr-48">
-                    <img className="drop-shadow-xl hover:scale-110 hover:rotate-[360deg] ease-in duration-1000" src={me} alt={"profile picture"}/>
+        <div className="grid min-h-screen bg-green-4 lg:grid-cols-2 lg:justify-center lg:items-center">
+                <div className="flex justify-center pt-20 lg:py-16 mx-16 lg:justify-end lg:mr-48">
+                    <img className="drop-shadow-xl h-fit hover:scale-110 hover:rotate-[360deg] ease-in duration-1000" src={me} alt={"profile picture"}/>
                 </div>
                 <div className="flex justify-center text-center lg:justify-start lg:flex-none lg:text-left mx-10 lg:m-0 lg:mr-28">
                     <span>
@@ -50,7 +49,5 @@ export default function LandingView() {
                     <ScrollLogo className="animate-bounce drop-shadow-xl"/>
                 </div>
             </div>
-        </div>
-
     )
 }
